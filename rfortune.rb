d10cc264be0f909b5941917064fc46ce05ed5b47
Alessandro Maestri <umpire274@gmail.com>
2025-07-30 01:46:38 +0200

    update v0.2.2

--- rfortune.rb
+++ rfortune.rb
@@ -5,16 +5,16 @@ class Rfortune < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/umpire274/rFortune/releases/download/v0.2.1/rfortune-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/umpire274/rFortune/releases/download/v0.2.2/rfortune-x86_64-apple-darwin.tar.gz"
       sha256 "eda8c76ea8a6f9883a523ac2730ba4e0ce12f009ba2cd0489aa3f46b8089a77e"
     elsif Hardware::CPU.arm?
-      url "https://github.com/umpire274/rFortune/releases/download/v0.2.1/rfortune-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/umpire274/rFortune/releases/download/v0.2.2/rfortune-aarch64-apple-darwin.tar.gz"
       sha256 "eac3e22619ffa3a98045cba88f34aa163a1548c8f0bc5b3dfaea83180f243df8"
     end
   end
 
   on_linux do
-    url "https://github.com/umpire274/rFortune/releases/download/v0.2.1/rfortune-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/umpire274/rFortune/releases/download/v0.2.2/rfortune-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "b16ca2b5dcc163bc84e5eebdb360fe13663948f17427a9777dde95977822fd41"
   end
 
